1b25bd3cfa3a0355a49634b601e1b6d797db5924
Apple <apple@Apples-MacBook-Pro.local>
2026-06-30 13:07:02 +0500

    Fix Netlify plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import { netlifyPlugin } from "@netlify/vite-plugin-react-router";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
   plugins: [
