6bb40af41ce9731d60fb34c237469fed099d68ab
Apple <apple@Apples-MacBook-Pro.local>
2026-06-30 12:54:40 +0500

    Configure Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { netlifyPlugin } from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    tailwindcss(), 
+    reactRouter(),
+    netlifyPlugin()
+  ],
   resolve: {
     tsconfigPaths: true,
   },
